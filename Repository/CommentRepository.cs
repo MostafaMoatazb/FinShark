0be875b930549a2ecc3f4ecfb2f3ed1970376a28
MostafaMoatazb <141503202+MostafaMoatazb@users.noreply.github.com>
2026-08-02 01:55:17 +0300

    Comment Repository

---
+++ Repository/CommentRepository.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Interfaces;
+using api.Models;
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace api.Repository
+{
+    public class CommentRepository : ICommentRepository
+    {
+        public readonly ApplicationDBContext _context;
+
+        public CommentRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Comment>> GetAllAsync()
+        {
+            return await _context.Comments.ToListAsync();
+        }
+    }
+}
